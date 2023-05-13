0a8cab196aa7b124ed3f5bf4d468f9ad97b10f17
ganesh <pganeshkumar.2010@gmail.com>
2023-05-13 20:36:38 +0530

    Added space in output message

---
+++ src/androidphone/BuyHeadset.java
@@ -0,0 +1,19 @@
+package androidphone;
+
+public class BuyHeadset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declaring variables 
+		int earphone; 
+        int earphonrpouch;
+        int totalcost;
+        //Assigning values to the variables
+        earphone = 450;
+        earphonrpouch = 120; 
+        //Calculating the total
+        totalcost = earphone + earphonrpouch;
+        System.out.println("Total cost of Headset is "+ totalcost);
+	}
+
+}
